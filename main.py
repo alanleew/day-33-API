50ce9b0916561ffe5d6b8371d2d7dd5a6f1120c3
alanleew <alanleew412@gmail.com>
2025-01-17 00:37:40 -0800

    Split up Kanye Quote API into separate document for readability

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
-###################################################################
-### Learning how to use API to get JSON data ###
-
 import requests
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# # print(response) # This gives back an HTTP [response code]
-# # print(response.status_code)
-# # print(response.raise_for_status()) # Uses Request module to raise errors for us
-#
-# json_data = response.json()
-# longitude = json_data["iss_position"]["longitude"]
-# latitude = json_data["iss_position"]["latitude"]
-#
+import datetime as dt
+
+MAR_VISTA_LAT = 34.019455
+MAR_VISTA_LONG = -118.491188
+
+### Learning how to use API to get JSON data ###
+# print(response) # This gives back an HTTP [response code]
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status() # Uses Request module to raise errors for us
+json_data = response.json()
+iss_longitude = float(json_data["iss_position"]["longitude"])
+iss_latitude = float(json_data["iss_position"]["latitude"])
 # iss_position = (longitude, latitude)
-# print(iss_position)
 
-###################################################################
+# Your position is within +-5 degrees of the ISS position
 
-# API_KANYE = "https://api.kanye.rest"
-#
-# from tkinter import *
-#
-# def get_quote():
-#     response = requests.get(API_KANYE)
-#     kanye_quote = response.json()["quote"]
-#     canvas.itemconfig(quote_text, text=kanye_quote)
-#
-# window = Tk()
-# window.title("Kanye Says...")
-# window.config(padx=50, pady=50)
-#
-# canvas = Canvas(width=300, height=414)
-# background_img = PhotoImage(file="background.png")
-# canvas.create_image(150, 207, image=background_img)
-# quote_text = canvas.create_text(150, 207, text="", width=250, font=("Arial", 30, "bold"), fill="white")
-# canvas.grid(row=0, column=0)
-#
-# kanye_img = PhotoImage(file="kanye.png")
-# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-# kanye_button.grid(row=1, column=0)
-#
-# window.mainloop()
+parameters = {
+    "lat": MAR_VISTA_LAT,
+    "lng": MAR_VISTA_LONG,
+    "formatted": 0
+}
 
-###################################################################
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = dt.datetime.now()
+
+# If the ISS is close to my current position
+# and it is currently dark
+# Send me an email to tell me to look up
+# BONUS: Run the code every 60 seconds
 
